579bc824f5d429a4391361c86bee389bed43a5a2
Glitch (tasty-foodz) <none>
2019-05-25 02:59:48 +0000

    Added basic CSS updates, slider, and form tweaks

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ app.use(express.urlencoded({ extended: true })); // for parsing application/x-ww
 
 app.use(express.static('public'))
 
+
+var date = new Date()
+var receivedDate = String(date.getMonth() + 1).padStart(2, '0') + '/' + String(date.getDate()).padStart(2, '0') + '/' + date.getFullYear()
+var responseDate = String(date.getMonth() + 1).padStart(2, '0') + '/' + String(date.getDate()+3).padStart(2, '0') + '/' + date.getFullYear()
+
 app.get('/kitties-are-great', (request, response) => {
   console.log('A page was loaded!', request.url)
   response.send('Kitties are great!')
@@ -13,7 +18,7 @@ app.get('/kitties-are-great', (request, response) => {
 
 app.post('/contact', (request, response) => {
   console.log('A post was submitted', request.body)
-  response.send(`Thanks ${request.body.name} for telling us "${request.body.message}"! You are awesome sauce.`)
+  response.send(`Thanks ${request.body.name} for telling us "${request.body.message}!" We received your message on ${receivedDate} and will respond by ${responseDate}. Or else.`)
 })
 
 app.listen(
